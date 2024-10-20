c749879364d6fb941ffddc59046a8860263ef73d
Taisson <taisson.oliveira@brave.ag>
2024-10-20 11:22:52 -0300

    Deployment file adjustment

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-// Export static Next Pages
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
     },
     assetPrefix: isProd ? '/portfolio/' : '',
     basePath: isProd ? '/portfolio/' : '',
-}
+};
 
+// Exporta a configuração usando sintaxe ES6
 export default nextConfig;
