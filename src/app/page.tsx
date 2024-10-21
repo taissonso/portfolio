0b3004377893d0b3a42938382f5f74b3832b08ba
Taisson <taisson.oliveira@brave.ag>
2024-10-21 08:54:19 -0300

    padding and font-size title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-full h-lvh">
-      <h1 className=" text-[48px] leading-[56px] lg:text-[72px] lg:leading-[50px] flex items-end gap-2">Em construção
+    <main className="flex items-center justify-center w-full h-lvh px-6">
+      <h1 className=" text-[36px] leading-[48px] lg:text-[72px] lg:leading-[50px] flex items-end gap-2">Em construção
         <div className="box-to-circles flex gap-2 ">
           <span className='w-3 h-3 rounded-[100%] bg-[#00592D] up-and-down delay-1'></span>
           <span className='w-3 h-3 rounded-[100%] bg-[#CD3742] up-and-down delay-2'></span>
