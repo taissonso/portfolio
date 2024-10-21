cb6118a8a6ab230a87a25b180fcc4e5310c615d6
Taisson <taisson.oliveira@brave.ag>
2024-10-21 08:43:05 -0300

    animation bullet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import './globals.css';
 export default function Home() {
   return (
     <main className="flex items-center justify-center w-full h-lvh">
-      <h1 className=" text-[48px] leading-[56px] lg:text-[72px] lg:leading-[50px]">Em construção...</h1>
+      <h1 className=" text-[48px] leading-[56px] lg:text-[72px] lg:leading-[50px] flex items-end gap-2">Em construção
+        <div className="box-to-circles flex gap-2 ">
+          <span className='w-3 h-3 rounded-[100%] bg-[#00592D] up-and-down delay-1'></span>
+          <span className='w-3 h-3 rounded-[100%] bg-[#CD3742] up-and-down delay-2'></span>
+          <span className='w-3 h-3 rounded-[100%] bg-[#E9C727] up-and-down delay-3'></span>
+        </div>
+
+
+      </h1>
     </main>
   );
 }
