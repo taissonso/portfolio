dd7c03e9f276c15ee242d25dd1a23858f4583607
Taisson <taisson.oliveira@brave.ag>
2024-10-24 22:14:49 -0300

    organizing folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-import './globals.css';
+import './styles/globals.css';
+import Image from 'next/image';
+import logo from '../app/public/images/logo-one.png';
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-full h-lvh px-6">
+    <main className="flex flex-col gap-6 items-center justify-center w-full h-lvh px-6">
+
+      <Image className='rounded-[120px]'
+        src={logo}
+        alt="Descrição da imagem"
+        width={600}
+        height={500}
+        priority
+      />
+
+
       <h1 className=" text-[36px] leading-[48px] lg:text-[72px] lg:leading-[50px] flex items-end gap-2">Em construção
         <div className="box-to-circles flex gap-2 ">
           <span className='w-3 h-3 rounded-[100%] bg-[#00592D] up-and-down delay-1'></span>
