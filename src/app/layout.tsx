af239d4f778a21d3a9f11f7a6f9c10c992cade00
Taisson <taisson.oliveira@brave.ag>
2025-01-27 21:21:51 -0300

    setup ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,36 @@
-import type { Metadata } from "next";
-import "./styles/globals.css";
+import { Metadata } from "next";
+import Script from "next/script";
 
+// Metadados estáticos
 export const metadata: Metadata = {
   title: "Taisson Oliveira - Desenvolvedor Web",
-  description: "Criando meu portfólio",
+  description: "Criando meu portfólio | WebDeveloper | FrontEnd",
+  alternates: {
+    canonical: "https://taissonso.github.io/portfolio/",
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}
-      >
+    <html lang="pt-BR">
+      <body>
+        {/* Google Analytics Scripts */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-B254J6KLR1"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-B254J6KLR1');
+            `,
+          }}
+        />
         {children}
       </body>
     </html>
