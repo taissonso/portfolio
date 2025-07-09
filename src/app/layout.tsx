06dff5d69c5b6217dca17865e62f05d96501940f
Taisson <taisson.oliveira@brave.ag>
2025-07-09 16:29:37 -0300

    adjust project flash home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <FontLoader />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Montserrat&display=swap" rel="stylesheet" />
       </head>
       <body className="bg-primary-dark font-manrope dark" suppressHydrationWarning>
         <ThemeProvider>
