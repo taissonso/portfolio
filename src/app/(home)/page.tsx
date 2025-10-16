ab30bc8da5970ed31bec2e4c58ce4578a20dde17
Taisson <taisson.oliveira@brave.ag>
2025-10-16 06:37:45 -0300

    update files

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,6 @@
 import { FC } from 'react';
 import HeroHome from '@/components/heros/home';
 import AboutMe from '@/components/sections/aboutMe';
-import FakeSection from '@/components/sections/fake';
 
 const Home: FC = () => {
 
